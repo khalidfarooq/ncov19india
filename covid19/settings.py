acb983c4fb5027fea4f2936e0676130759e1f098
khalid farooq <47777912+khalidfarooq@users.noreply.github.com>
2020-06-09 22:51:56 +0530

    Update settings.py

--- covid19/settings.py
+++ covid19/settings.py
@@ -119,13 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,"live-static-files","static-root")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
