80ae093f320e6e6244705d8751c1346342d1ed78
khalid farooq <47777912+khalidfarooq@users.noreply.github.com>
2020-06-09 22:47:40 +0530

    Update settings.py

--- covid19/settings.py
+++ covid19/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,"live-static-files","static-root")
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
