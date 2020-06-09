06d5c63ef406a11772cf6d4319461ec36e47c95e
khalid farooq <47777912+khalidfarooq@users.noreply.github.com>
2020-06-09 23:25:13 +0530

    Update totalcasech.js

--- static/js/totalcasech.js
+++ static/js/totalcasech.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var url = "http://api.covid19india.org/data.json";
+  var url = "https://api.covid19india.org/data.json";
 
   $.getJSON(url, function (data) {
     //TOTAL CASES
